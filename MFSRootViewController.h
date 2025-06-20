ead326c9c2432f1f9fdabff9ce332e2317ae6d04
PaTTeeL <PaTTeeL@users.noreply.github.com>
2025-06-21 06:49:12 +0800

    Replace Private Preferences Framework with Standard UIKit Component

--- MFSRootViewController.h
+++ MFSRootViewController.h
@@ -1,7 +1,15 @@
 #import <UIKit/UIKit.h>
-#import <Preferences/PSListController.h>
-#import <Preferences/PSSpecifier.h>
+//#import <Preferences/PSListController.h>
+//#import <Preferences/PSSpecifier.h>
 
-@interface MFSRootViewController : PSListController
+//@interface MFSRootViewController : PSListController
+
+// 将父类改为 UIViewController，添加 UITableViewDataSource 和 UITableViewDelegate 协议
+@interface MFSRootViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+// 添加 UITableView 属性
+@property (nonatomic, strong) UITableView *tableView;
+// 添加用于存储应用数据的属性
+@property (nonatomic, strong) NSMutableArray *dataSource;
 
 @end
